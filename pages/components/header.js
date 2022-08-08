a84eabff590394700c197ff7b801ad631d1b8571
Ibrahim-AlKalaileh <Ibrahim.al.kalaileh@gmail.com>
2022-08-08 06:35:32 +0300

    added google fonts and icons

--- pages/components/header.js
+++ pages/components/header.js
@@ -1,3 +1,5 @@
+import { ImFacebook,ImTwitter,ImYoutube } from "react-icons/im";
+
 export default function header() {
   return (
     <header className="bg-gray-50">
@@ -7,13 +9,13 @@ export default function header() {
         </div>
 
         <div className="shrink w-80 sm:order-2">
-          <a>Design</a>
+          <a className="font-bold uppercase text-3xl">Design</a>
         </div>
         <div className="w-96 order-3 flex justify-center">
           <div className="flex gap-6">
-          <a>Facebook</a>
-          <a>Twitter</a>
-          <a>Youtube</a>
+          <a><ImFacebook color="#888888"/></a>
+          <a><ImTwitter color="#888888"/></a>
+          <a><ImYoutube color="#888888"/></a>
           </div>
         </div>
         
