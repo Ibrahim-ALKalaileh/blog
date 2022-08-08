b10e6ef12d36c6d40f3835f6f2f787a3f0843e2d
Ibrahim-AlKalaileh <Ibrahim.al.kalaileh@gmail.com>
2022-08-08 07:05:16 +0300

    added layout and trending section

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from './components/header'
+import Format from './layout/format'
+// components
+import Section1 from './components/section1'
 export default function Home() {
   return (
-    <div>
-      <Header></Header>
-    </div>
+    <Format>
+      <Section1></Section1>
+    </Format>
   )
 }
