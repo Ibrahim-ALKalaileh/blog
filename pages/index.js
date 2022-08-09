a88f1a0661d1b32b9acc678df082d3a9183bd9ab
Ibrahim-AlKalaileh <Ibrahim.al.kalaileh@gmail.com>
2022-08-09 04:03:27 +0300

    added post page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
-import Format from "./layout/format";
+import Format from "../layout/format";
 // components
-import Trending from "./components/trending";
-import Latest_posts from "./components/latest_posts";
-import Most_popular from "./components/most_popular";
-import CategorizedPostsSec from "./components/categorizedPostsSec";
+import Trending from "../components/trending";
+import Latest_posts from "../components/latest_posts";
+import Most_popular from "../components/most_popular";
+import CategorizedPostsSec from "../components/categorizedPostsSec";
 export default function Home() {
   return (
     <Format>
