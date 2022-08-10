8b2f9835994150259b07b2802551d733faea4bd0
Ibrahim-AlKalaileh <Ibrahim.al.kalaileh@gmail.com>
2022-08-10 08:58:33 +0300

    linked related posts to api and finalized the blog

--- components/_child/relatedPosts.js
+++ components/_child/relatedPosts.js
@@ -1,29 +1,38 @@
 import Link from "next/link";
 import Image from "next/image";
 import Author from "./author";
+import LoadingSpinner from "./loadingSpinner";
+import Error from "./error";
+import fetcher
+ from "../../lib/fethcer";
 export default function RelatedPosts() {
+
+  const {data,isLoading,isError}=fetcher('api/posts')
+  
+  if(isLoading) return <LoadingSpinner></LoadingSpinner>
+  if(isError) return <Error></Error>
+
   return (
     <section className="pt-20">
       <h1 className="font-bold text-3xl py-10">Related</h1>
 
       <div className="flex flex-col gap-10">
-        {Posts()}
-        {Posts()}
-        {Posts()}
-        {Posts()}
-        {Posts()}
+        {data.map((value)=>(
+          <Posts data={value} key={value.id}></Posts>
+        ))}
       </div>
     </section>
   );
 }
-function Posts() {
+function Posts({data}) {
+  const {id,title,subtitle,category,img,published,author}=data;
   return (
     <div className="flex gap-5">
       <div className="image flex flex-col justify-start">
-        <Link href={"/"}>
+        <Link href={`/posts/${id}`}>
           <a>
             <Image
-              src={"/images/img1.jpg"}
+              src={img}
               className="rounded"
               width={300}
               height={200}
@@ -33,20 +42,20 @@ function Posts() {
       </div>
       <div className="info flex justify-center flex-col">
         <div className="cat">
-          <Link href={"/"}>
+          <Link href={`/posts/${id}`}>
             <a className="text-orange-600 hover:text-orange-800">
-              Bussiness,Travel
+              {category}
             </a>
           </Link>
-          <Link href={"/"}>
-            <a className="text-gray-800 hover:text-gray-600">- August 8,2022</a>
+          <Link href={`/posts/${id}`}>
+            <a className="text-gray-800 hover:text-gray-600">- {published}</a>
           </Link>
         </div>
 
         <div className="title">
-          <Link href={"/"}>
+          <Link href={`/posts/${id}`}>
             <a className="text-xl font-bold text-gray-800 hover:text-gray-600">
-              woow this is my first post
+              {title}
             </a>
           </Link>
         </div>
